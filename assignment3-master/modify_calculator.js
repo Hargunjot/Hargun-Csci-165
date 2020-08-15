ef60f13ed7d2bc08accbad2858df37507e6e10d7
Hargunjot <66041124+Hargunjot@users.noreply.github.com>
2020-08-15 14:35:22 -0700

    Add files via upload

---
+++ assignment3-master/modify_calculator.js
@@ -0,0 +1,63 @@
+var first=0;
+var second=0;
+
+add = function() 
+{ 
+    first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)+parseInt(second);
+    if(first & second){
+        jQuery('#result').html(first+"+"+second+ "=" + (result));
+        jQuery('#history').append("<li></li>"+first+"+"+second+ "=" + (result));
+    }
+    
+    
+}
+sub = function() 
+{   first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)-parseInt(second);
+
+    if(first & second){
+    jQuery('#result').html(first+"-"+second+ "=" + (result));
+    jQuery('#history').append("<li></li>"+first+"-"+second+ "=" + (result)+ "\n");
+    }
+}
+mul = function() 
+{   first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)*parseInt(second);
+
+    if(first & second){
+    jQuery('#result').html(first+"*"+second+ "=" + (result));
+    jQuery('#history').append("<li></li>"+first+"*"+second+ "=" + (result)+ "\n");
+    }
+}
+div = function() 
+{   first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)/parseInt(second);
+    if(first & second){
+    jQuery('#result').html(first+"/"+second+ "=" + (result));
+    jQuery('#history').append("<li></li>"+first+"/"+second+ "=" + (result)+ "\n");
+    }
+}
+clear=function()
+{
+    result=" ";
+    jQuery('#result').html(result);
+    jQuery('#history').html(result);
+}
+
+setup = function() {    
+    jQuery('#add_button').click(add)
+    jQuery('#add_button').mouseover(add)
+    jQuery('#sub_button').click(sub)
+    jQuery('#sub_button').mouseover(sub)
+    jQuery('#mul_button').click(mul)
+    jQuery('#mul_button').mouseover(mul)
+    jQuery('#div_button').click(div)
+    jQuery('#div_button').mouseover(div)
+    jQuery('#clear_button').click(clear)
+}
+jQuery(document).ready(setup)
